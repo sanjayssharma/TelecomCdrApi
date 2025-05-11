7d43102c4e8764b5ce3564ceb9d4848e6fccd6e1
Sanjay Sharma <Sanjay.sharma@giacom.com>
2025-05-11 18:48:29 +0100

    Add central package management support

---
+++ PETechTest/TelecomCdr.API/Controllers/CdrController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TelecomCdr.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CdrController : ControllerBase
+    {
+        private readonly ILogger<CdrController> _logger;
+
+        public CdrController(ILogger<CdrController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
